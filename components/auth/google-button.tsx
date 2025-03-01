2bd2d4bc9abbc0a9dec23e54b5e43a7ea6262ff4
krid <luckykrid1994@gmail.com>
2025-03-01 15:33:52 +0700

    test

--- components/auth/google-button.tsx
+++ components/auth/google-button.tsx
@@ -14,7 +14,7 @@ const GoogleLoginButton = () => {
       <button className="flex items-center gap-3 w-full text-white px-[0.5%] py-[3%]">
         <Image className="rounded-full absolute " alt="google login" src={"/auth/googleIcon.svg"} width={40} height={40} />
   
-        <div className="flex justify-center w-[100%] font-bold"> Continue with Google</div>
+        <div className="flex justify-center w-[100%] font-bold"> Continue with Googles</div>
       </button>
       <p>{errorMsgGoogle}</p>
     </form>
